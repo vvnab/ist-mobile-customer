f833d36439ac3819f0dfa3bc353b1a8059bb38dd
Виталий Набережный <naberezhniy@taxi21.ru>
2016-04-01 18:18:35 +0300

    ---

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 
-var DEBUG = false;
+var DEBUG = true;
 var GEOLOCATION_TIMEOUT = 5000;
 var GEOLOCATION_ACCURACY = 50;
 var ARC_ORDERS_WEEKS = 52;
